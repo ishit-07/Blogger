1dbe0f086e88e5a57503344ce901878c3c7e53a5
Ishit verma <ishitverma2602@gmail.com>
2023-06-04 13:16:55 +0530

    Update create.js

--- src/create.js
+++ src/create.js
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-// there is an useHistory hook in react router dom
-// it will redirect to a new route
 import { useHistory } from "react-router-dom";
 
 const Create = () => {
@@ -11,18 +9,17 @@ const Create = () => {
   const [isPending, setIsPending] = useState(false);
 
 
-  // first we have to call useHistory
+
   const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // for adding inside the data.json we have to store the value inside an object
+   
     const blog = { title, body , author };
 
     setIsPending(true);
 
-    // for adding into json server we have to use a fetch request
     fetch('http://localhost:8000/blogs' , {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
@@ -33,18 +30,6 @@ const Create = () => {
         setIsPending(false);
       },300)
 
-
-      // this is how we can route to the next page in javascript
-      // window.location.replace('/');
-
-      // if you have to go some pages back or front but this not very usable
-      // this is for back
-      // history.go(-1);
-      // this is for front
-      // this is will go two pages front
-      // history.go(2);
-
-      // we have to use this to route at any page
       history.push('/');
     })
   }
@@ -77,10 +62,6 @@ const Create = () => {
         {!isPending && <button>Add Blog</button>}
         { isPending && <button>Adding Blog...</button>} 
       </form>
-
-      {/* <p>{ title }</p>
-      <p>{ body }</p>
-      <p>{ author }</p> */}
     </div>
   );
 };
